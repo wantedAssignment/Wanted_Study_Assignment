c208effab1aed7666780ce8128bdfe036230a6e0
sean <tmddnr3163@gmail.com>
2021-07-09 23:47:28 +0900

    2021/07/09 Setup PJT

---
+++ src/Components/Naviagation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = (props) => {
+  return <nav>Hi</nav>;
+};
+
+export default Navigation;
---
+++ src/Routers/App.js
@@ -0,0 +1,11 @@
+import Naviagation from '../Components/Naviagation';
+
+function App() {
+  return (
+    <div className="App">
+      <Naviagation />
+    </div>
+  );
+}
+
+export default App;

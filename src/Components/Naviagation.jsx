92b27958036fa2ec3e92877fcf8f795e38f6204e
sean <tmddnr3163@gmail.com>
2021-07-10 02:31:51 +0900

    2021/07/10 Navigation Components

--- src/Components/Naviagation.jsx
+++ src/Components/Naviagation.jsx
@@ -1,7 +1,143 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    position: 'fixed',
+    backgroundColor: '#fff',
+    boxShadow: '0 1px 0 0 rgb(0 0 0 / 10%);',
+    padding: '10px',
+    color: '#333333',
+    fontSize: '14px',
+    fontWeight: '600',
+  },
+  nav: {
+    display: 'flex',
+    alignItems: 'center',
+    maxWidth: '1000px',
+  },
+  section: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  logo: {
+    fontSize: '25px',
+    fontWeight: 'bold',
+    transition: 'all 0.5s ease-in-out',
+    '&:hover': {
+      color: '#585858',
+    },
+    cursor: 'pointer',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '10px',
+    },
+  },
+  ul: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    position: 'relative',
+    [theme.breakpoints.down('sm')]: {
+      width: '50%',
+    },
+  },
+  small: {
+    position: 'absolute',
+    color: '#3366FF',
+    fontSize: '3px',
+    right: '-5px',
+    top: '-5px',
+  },
+  signup: {
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '10px',
+    },
+  },
+  circle: {
+    border: '0.5px solid #c5c5c5',
+    borderRadius: '10px',
+    color: '#878787',
+    padding: '5px',
+    cursor: 'pointer',
+  },
+  sm: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  md: {
+    [theme.breakpoints.down('md')]: {
+      display: 'none',
+    },
+  },
+}));
 
 const Navigation = (props) => {
-  return <nav>Hi</nav>;
+  const classes = useStyles();
+  const theme = useTheme();
+  console.log(theme);
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} className={classes.nav}>
+        <Grid item xs={8} sm={8} md={8}>
+          <section className={classes.section}>
+            <Grid container className={classes.section}>
+              <Grid item xs={12} sm={12} md={2}>
+                <div className={classes.logo}>wanted</div>
+              </Grid>
+              <Grid item xs={12} sm={12} md={10}>
+                <ul className={classes.ul}>
+                  <li className={classes.sm}>홈</li>
+                  <li>탐색</li>
+                  <li>커리어 성장</li>
+                  <li className={classes.md}>직군별 연봉</li>
+                  <li className={classes.md}>이력서</li>
+                  <li className={classes.md}>매치업</li>
+                  <li className={classes.md}>프리랜서</li>
+                  <li className={classes.md}>Ai 합격예측</li>
+                  <small className={`${classes.small} ${classes.md}`}>
+                    Beta
+                  </small>
+                </ul>
+              </Grid>
+            </Grid>
+          </section>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4}>
+          <Grid container className={classes.section}>
+            <Grid item xs={12} sm={12} md={5} className={classes.section}>
+              <span className={classes.md}>
+                <SearchIcon />
+              </span>
+              <span className={classes.md}> 회원가입/로그인</span>
+              <span className={`${classes.sm} ${classes.signup}`}>
+                회원가입하기
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={12} md={5} className={classes.section}>
+              <span className={`${classes.circle} ${classes.md}`}>
+                기업 서비스
+              </span>
+              <div className={`${classes.section} ${classes.sm}`}>
+                <SearchIcon />
+                <MenuIcon />
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Navigation;
--- src/Routers/App.js
+++ src/Routers/App.js
@@ -1,8 +1,12 @@
 import Naviagation from '../Components/Naviagation';
+import { Global } from '@emotion/react';
+//
+import Reset from '../Components/Reset';
 
 function App() {
   return (
     <div className="App">
+      <Global styles={Reset} />
       <Naviagation />
     </div>
   );
